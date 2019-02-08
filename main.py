e2d638ae25c930fb9de5822915dea61554bef4ef
Naeem Akhtar <moderngabbar@gmail.com>
2019-02-08 17:30:39 +0530

    bugs fixes and functions seperations

--- main.py
+++ main.py
@@ -1,67 +1,14 @@
 #dependencies
-from bs4 import BeautifulSoup
-import requests
-import re
 import operator
 import json
 from tabulate import tabulate
 import sys
-from stop_words import get_stop_words
-
-#clean word with regex
-def clean_word(word):
-    cleaned_word = re.sub('[^A-Za-z]+', '', word)
-    return cleaned_word
+from functions import *	# all functions reside in this file
 
 
-# get the words
-def getWordList(url):
-	word_list = []
-	#taking web page data in xlml
-	source_code = requests.get(url)
-	plain_text = source_code.text
-	soup = BeautifulSoup(plain_text, 'lxml')
-
-	#finding paragraph text
-	for text in soup.findAll('p'):
-		if text.text is None:
-			continue
-		content = text.text
-		words = content.lower().split()
-
-		#each word in para
-		for word in words:
-			cleaned_word = clean_word(word)
-			if len(cleaned_word) > 0:
-				word_list.append(cleaned_word)
-	return word_list
-
-
-def createFrequencyTable(word_list):
-	#word count
-	word_count = {}
-	for word in word_list:
-		if word in word_count:
-			word_count[word] += 1
-		else:
-			word_count[word] = 1
-	return word_count
-
-
-#remove stop words
-def remove_stop_words(frequency_list):
-    stop_words = get_stop_words('en')
-    temp_list = []
-    for key,value in frequency_list:
-        if key not in stop_words:
-            temp_list.append([key, value])
-    return temp_list
-
-
-#main code starts here
 #get data from wiki
 wikipedia_api_link = 	"http://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch="
-wikipedia_link = "http://en.wikipedia.org/wiki"
+wikipedia_link = "http://en.wikipedia.org/wiki/"
 
 if(len(sys.argv) < 2):
 	print("Enter a valid string")
@@ -81,11 +28,19 @@ try:
 	response = requests.get(url)
 	data = json.loads(response.content.decode('utf-8'))
 	#format data
-	wikipedia_page_tag = data['query']['search'][0]['title']
-	#create new url
-	url = wikipedia_link + wikipedia_page_tag
-	
-	page_word_list = getWordList(url)
+
+	page_word_list = []
+	print("Scrapping Wikipedia pages..")
+	search_result = data['query']['search']
+	if(len(search_result) < 1):
+		print("No search result, try checking the spelling.")
+	for i in range(len(search_result)):
+		wikipedia_page_tag = search_result[i]['title']
+		#create new url
+		url = wikipedia_link + wikipedia_page_tag
+		page_word_list.extend(getWordList(url) + wikipedia_page_tag.split()) # getWordList(url) --> Takes url and scrap all the words from <p> tags of page.
+		print(url)
+
 	#create table of word counts
 	page_word_counts = createFrequencyTable(page_word_list)
 	sorted_word_frequency_list = sorted(page_word_counts.items(), key=operator.itemgetter(1), reverse=True)
@@ -102,6 +57,7 @@ try:
 	#just get top 20 words
 	if len(sorted_word_frequency_list) > 20:
 		sorted_word_frequency_list = sorted_word_frequency_list[:20]
+	print("\nTop 20 words related to {}.\n".format(string_query))
 
 	#create our final list, words + frequency + percentage
 	final_list = []
@@ -113,7 +69,6 @@ try:
 
 	#print the table with tabulate
 	print(tabulate(final_list, headers=print_headers, tablefmt='orgtbl'))
-
 
 #throw an exception in case it breaks
 except requests.exceptions.Timeout:
